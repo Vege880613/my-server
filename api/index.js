43d1de1c24bf78cdbc79426f5b80882846b52b55
Vege880613 <137055778@qq.com>
2026-09-23 09:32:04 +0800

    修复路径

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const Record = require('./models/Record');
+const Record = require('../models/Record');
 
 const app = express();
 
